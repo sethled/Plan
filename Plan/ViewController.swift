aa3cb253e5f37ec8680e9a7fa2f36c8589ed9159
Seth Ledford <sethled@Seths-MacBook-Pro.local>
2022-07-20 18:20:46 -0400

    Very little progress. Frustrated.

--- Plan/ViewController.swift
+++ Plan/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct CellData{
+    let text : String?
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var tempDataArray = ["Data0", "Data1", "Data2"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempDataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "theCell")! as UITableViewCell
+        
+        myCell.textLabel!.text = tempDataArray[indexPath.row]
+        
+        return myCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "theCell")
+        tableView.dataSource = self
     }
 
 
